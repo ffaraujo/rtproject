aabf529a1ed77258dd314e244d1ee2f010dc3f46
Fabio Feliciano <fabio.flc@gmail.com>
2015-03-23 11:27:25 -0300

    * Ajustes de configuração de banco de dados; * Criação da primeira versão do read me.
    Signed-off-by: Fabio Feliciano <fabio.flc@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-READ ME
+## README
+
+### TOOLS
+
+Tools used to build application. Ideally you should use same versions or higher to sure it works properly.
+
+* PHP version: 5.3.13
+* MySQL version: 5.5.24
+* Apache: 2.2.22
+
+### INSTRUCTIONS
+
+* You should create a database called: **rtproject** and a db user called **rtuser** with password **rtu53r**
+* And then should run db/model.sql script to populate the new db.
+* One user with high privilegies was created (login: *admin@example.com* / password: *123123*), you should change both as soon as possible.
+* Index file is missing, this is deliberate, but you can request it from me anytime
+
+### CONTACT
+
+Fabio Feliciano (<fabio.flc@gmail.com>)
--- db/model.sql
+++ db/model.sql
@@ -2,13 +2,13 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-CREATE SCHEMA IF NOT EXISTS `basedb` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
-USE `basedb` ;
+CREATE SCHEMA IF NOT EXISTS `rtproject` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `rtproject` ;
 
 -- -----------------------------------------------------
--- Table `basedb`.`system`
+-- Table `rtproject`.`system`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `basedb`.`system` (
+CREATE TABLE IF NOT EXISTS `rtproject`.`system` (
   `sys_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `sys_name` VARCHAR(80) NOT NULL,
   `sys_value` VARCHAR(80) NULL DEFAULT NULL,
@@ -19,9 +19,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `basedb`.`acl_roles`
+-- Table `rtproject`.`acl_roles`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `basedb`.`acl_roles` (
+CREATE TABLE IF NOT EXISTS `rtproject`.`acl_roles` (
   `rol_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `rol_desc` VARCHAR(30) NOT NULL,
   `rol_active` TINYINT NOT NULL DEFAULT 1,
@@ -30,9 +30,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `basedb`.`users`
+-- Table `rtproject`.`users`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `basedb`.`users` (
+CREATE TABLE IF NOT EXISTS `rtproject`.`users` (
   `use_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `use_name` VARCHAR(80) NULL,
   `use_email` VARCHAR(150) NOT NULL,
@@ -48,21 +48,21 @@ CREATE TABLE IF NOT EXISTS `basedb`.`users` (
   INDEX `fk_users_users1_idx` (`use_parent` ASC),
   CONSTRAINT `fk_usuarios_acl_roles1`
     FOREIGN KEY (`use_role`)
-    REFERENCES `basedb`.`acl_roles` (`rol_id`)
+    REFERENCES `rtproject`.`acl_roles` (`rol_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_users_users1`
     FOREIGN KEY (`use_parent`)
-    REFERENCES `basedb`.`users` (`use_id`)
+    REFERENCES `rtproject`.`users` (`use_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `basedb`.`acl_resources`
+-- Table `rtproject`.`acl_resources`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `basedb`.`acl_resources` (
+CREATE TABLE IF NOT EXISTS `rtproject`.`acl_resources` (
   `res_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `res_desc` VARCHAR(30) NOT NULL,
   `res_active` TINYINT NOT NULL DEFAULT 1,
@@ -71,9 +71,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `basedb`.`acl_privileges`
+-- Table `rtproject`.`acl_privileges`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `basedb`.`acl_privileges` (
+CREATE TABLE IF NOT EXISTS `rtproject`.`acl_privileges` (
   `pri_id` VARCHAR(4) NOT NULL,
   `pri_desc` VARCHAR(30) NOT NULL,
   `pri_active` TINYINT UNSIGNED NOT NULL DEFAULT 1,
@@ -82,9 +82,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `basedb`.`acl_access`
+-- Table `rtproject`.`acl_access`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `basedb`.`acl_access` (
+CREATE TABLE IF NOT EXISTS `rtproject`.`acl_access` (
   `acc_resource` INT UNSIGNED NOT NULL,
   `acc_role` INT UNSIGNED NOT NULL,
   `acc_privilege` VARCHAR(4) NOT NULL,
@@ -94,26 +94,26 @@ CREATE TABLE IF NOT EXISTS `basedb`.`acl_access` (
   INDEX `fk_acl_access_acl_privileges1_idx` (`acc_privilege` ASC),
   CONSTRAINT `fk_acl_access_acl_resources`
     FOREIGN KEY (`acc_resource`)
-    REFERENCES `basedb`.`acl_resources` (`res_id`)
+    REFERENCES `rtproject`.`acl_resources` (`res_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_acl_access_acl_roles1`
     FOREIGN KEY (`acc_role`)
-    REFERENCES `basedb`.`acl_roles` (`rol_id`)
+    REFERENCES `rtproject`.`acl_roles` (`rol_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_acl_access_acl_privileges1`
     FOREIGN KEY (`acc_privilege`)
-    REFERENCES `basedb`.`acl_privileges` (`pri_id`)
+    REFERENCES `rtproject`.`acl_privileges` (`pri_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `basedb`.`auditorship`
+-- Table `rtproject`.`auditorship`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `basedb`.`auditorship` (
+CREATE TABLE IF NOT EXISTS `rtproject`.`auditorship` (
   `aud_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `aud_user` INT UNSIGNED NOT NULL,
   `aud_action` VARCHAR(100) NOT NULL,
@@ -129,89 +129,89 @@ SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 -- -----------------------------------------------------
--- Data for table `basedb`.`system`
+-- Data for table `rtproject`.`system`
 -- -----------------------------------------------------
 START TRANSACTION;
-USE `basedb`;
-INSERT INTO `basedb`.`system` (`sys_id`, `sys_name`, `sys_value`, `sys_desc`, `sys_active`) VALUES (1, 'online', '1', 'define se o site esta online ou offline', 0);
-INSERT INTO `basedb`.`system` (`sys_id`, `sys_name`, `sys_value`, `sys_desc`, `sys_active`) VALUES (2, 'delete-mode', '0', 'define se o modo de delecao e real ou logico', 0);
-INSERT INTO `basedb`.`system` (`sys_id`, `sys_name`, `sys_value`, `sys_desc`, `sys_active`) VALUES (3, 'expiration-time', '3600', 'define o tempo em segundos da sessao de usuario', 0);
-INSERT INTO `basedb`.`system` (`sys_id`, `sys_name`, `sys_value`, `sys_desc`, `sys_active`) VALUES (4, 'log-mode', 'text', 'define se o log sera salvo em texto ou no banco', 0);
-INSERT INTO `basedb`.`system` (`sys_id`, `sys_name`, `sys_value`, `sys_desc`, `sys_active`) VALUES (5, 'multi-language', '0', 'define se o site tem mais de um idioma', 0);
+USE `rtproject`;
+INSERT INTO `rtproject`.`system` (`sys_id`, `sys_name`, `sys_value`, `sys_desc`, `sys_active`) VALUES (1, 'online', '1', 'define se o site esta online ou offline', 0);
+INSERT INTO `rtproject`.`system` (`sys_id`, `sys_name`, `sys_value`, `sys_desc`, `sys_active`) VALUES (2, 'delete-mode', '0', 'define se o modo de delecao e real ou logico', 0);
+INSERT INTO `rtproject`.`system` (`sys_id`, `sys_name`, `sys_value`, `sys_desc`, `sys_active`) VALUES (3, 'expiration-time', '3600', 'define o tempo em segundos da sessao de usuario', 0);
+INSERT INTO `rtproject`.`system` (`sys_id`, `sys_name`, `sys_value`, `sys_desc`, `sys_active`) VALUES (4, 'log-mode', 'text', 'define se o log sera salvo em texto ou no banco', 0);
+INSERT INTO `rtproject`.`system` (`sys_id`, `sys_name`, `sys_value`, `sys_desc`, `sys_active`) VALUES (5, 'multi-language', '0', 'define se o site tem mais de um idioma', 0);
 
 COMMIT;
 
 
 -- -----------------------------------------------------
--- Data for table `basedb`.`acl_roles`
+-- Data for table `rtproject`.`acl_roles`
 -- -----------------------------------------------------
 START TRANSACTION;
-USE `basedb`;
-INSERT INTO `basedb`.`acl_roles` (`rol_id`, `rol_desc`, `rol_active`) VALUES (1, 'ROOT', 1);
-INSERT INTO `basedb`.`acl_roles` (`rol_id`, `rol_desc`, `rol_active`) VALUES (2, 'ADMIN', 1);
-INSERT INTO `basedb`.`acl_roles` (`rol_id`, `rol_desc`, `rol_active`) VALUES (3, 'USER', 1);
+USE `rtproject`;
+INSERT INTO `rtproject`.`acl_roles` (`rol_id`, `rol_desc`, `rol_active`) VALUES (1, 'ROOT', 1);
+INSERT INTO `rtproject`.`acl_roles` (`rol_id`, `rol_desc`, `rol_active`) VALUES (2, 'ADMIN', 1);
+INSERT INTO `rtproject`.`acl_roles` (`rol_id`, `rol_desc`, `rol_active`) VALUES (3, 'USER', 1);
 
 COMMIT;
 
 
 -- -----------------------------------------------------
--- Data for table `basedb`.`users`
+-- Data for table `rtproject`.`users`
 -- -----------------------------------------------------
 START TRANSACTION;
-USE `basedb`;
-INSERT INTO `basedb`.`users` (`use_id`, `use_name`, `use_email`, `use_password`, `use_image`, `use_insert_date`, `use_last_access`, `use_role`, `use_parent`, `use_active`) VALUES (1, 'ADMIN', 'fabio.flc@gmail.com', sha1('123123'), NULL, NOW(), NULL, 1, NULL, 1);
+USE `rtproject`;
+INSERT INTO `rtproject`.`users` (`use_id`, `use_name`, `use_email`, `use_password`, `use_image`, `use_insert_date`, `use_last_access`, `use_role`, `use_parent`, `use_active`) VALUES (1, 'ADMIN', 'admin@example.com', sha1('123123'), NULL, NOW(), NULL, 1, NULL, 1);
 
 COMMIT;
 
 
 -- -----------------------------------------------------
--- Data for table `basedb`.`acl_resources`
+-- Data for table `rtproject`.`acl_resources`
 -- -----------------------------------------------------
 START TRANSACTION;
-USE `basedb`;
-INSERT INTO `basedb`.`acl_resources` (`res_id`, `res_desc`, `res_active`) VALUES (1, 'users', 1);
-INSERT INTO `basedb`.`acl_resources` (`res_id`, `res_desc`, `res_active`) VALUES (2, 'system', 1);
-INSERT INTO `basedb`.`acl_resources` (`res_id`, `res_desc`, `res_active`) VALUES (3, 'auditorship', 1);
-INSERT INTO `basedb`.`acl_resources` (`res_id`, `res_desc`, `res_active`) VALUES (4, 'access', 1);
+USE `rtproject`;
+INSERT INTO `rtproject`.`acl_resources` (`res_id`, `res_desc`, `res_active`) VALUES (1, 'users', 1);
+INSERT INTO `rtproject`.`acl_resources` (`res_id`, `res_desc`, `res_active`) VALUES (2, 'system', 1);
+INSERT INTO `rtproject`.`acl_resources` (`res_id`, `res_desc`, `res_active`) VALUES (3, 'auditorship', 1);
+INSERT INTO `rtproject`.`acl_resources` (`res_id`, `res_desc`, `res_active`) VALUES (4, 'access', 1);
 
 COMMIT;
 
 
 -- -----------------------------------------------------
--- Data for table `basedb`.`acl_privileges`
+-- Data for table `rtproject`.`acl_privileges`
 -- -----------------------------------------------------
 START TRANSACTION;
-USE `basedb`;
-INSERT INTO `basedb`.`acl_privileges` (`pri_id`, `pri_desc`, `pri_active`) VALUES ('A', 'ACTIVATE', 1);
-INSERT INTO `basedb`.`acl_privileges` (`pri_id`, `pri_desc`, `pri_active`) VALUES ('C', 'CREATE', 1);
-INSERT INTO `basedb`.`acl_privileges` (`pri_id`, `pri_desc`, `pri_active`) VALUES ('D', 'DELETE', 1);
-INSERT INTO `basedb`.`acl_privileges` (`pri_id`, `pri_desc`, `pri_active`) VALUES ('R', 'READ', 1);
-INSERT INTO `basedb`.`acl_privileges` (`pri_id`, `pri_desc`, `pri_active`) VALUES ('U', 'UPDATE', 1);
+USE `rtproject`;
+INSERT INTO `rtproject`.`acl_privileges` (`pri_id`, `pri_desc`, `pri_active`) VALUES ('A', 'ACTIVATE', 1);
+INSERT INTO `rtproject`.`acl_privileges` (`pri_id`, `pri_desc`, `pri_active`) VALUES ('C', 'CREATE', 1);
+INSERT INTO `rtproject`.`acl_privileges` (`pri_id`, `pri_desc`, `pri_active`) VALUES ('D', 'DELETE', 1);
+INSERT INTO `rtproject`.`acl_privileges` (`pri_id`, `pri_desc`, `pri_active`) VALUES ('R', 'READ', 1);
+INSERT INTO `rtproject`.`acl_privileges` (`pri_id`, `pri_desc`, `pri_active`) VALUES ('U', 'UPDATE', 1);
 
 COMMIT;
 
 
 -- -----------------------------------------------------
--- Data for table `basedb`.`acl_access`
+-- Data for table `rtproject`.`acl_access`
 -- -----------------------------------------------------
 START TRANSACTION;
-USE `basedb`;
-INSERT INTO `basedb`.`acl_access` (`acc_resource`, `acc_role`, `acc_privilege`, `acc_allow`) VALUES (1, 1, 'A', 1);
-INSERT INTO `basedb`.`acl_access` (`acc_resource`, `acc_role`, `acc_privilege`, `acc_allow`) VALUES (1, 1, 'C', 1);
-INSERT INTO `basedb`.`acl_access` (`acc_resource`, `acc_role`, `acc_privilege`, `acc_allow`) VALUES (1, 1, 'D', 1);
-INSERT INTO `basedb`.`acl_access` (`acc_resource`, `acc_role`, `acc_privilege`, `acc_allow`) VALUES (1, 1, 'R', 1);
-INSERT INTO `basedb`.`acl_access` (`acc_resource`, `acc_role`, `acc_privilege`, `acc_allow`) VALUES (1, 1, 'U', 1);
-INSERT INTO `basedb`.`acl_access` (`acc_resource`, `acc_role`, `acc_privilege`, `acc_allow`) VALUES (2, 1, 'U', 1);
-INSERT INTO `basedb`.`acl_access` (`acc_resource`, `acc_role`, `acc_privilege`, `acc_allow`) VALUES (1, 2, 'A', 1);
-INSERT INTO `basedb`.`acl_access` (`acc_resource`, `acc_role`, `acc_privilege`, `acc_allow`) VALUES (1, 2, 'C', 1);
-INSERT INTO `basedb`.`acl_access` (`acc_resource`, `acc_role`, `acc_privilege`, `acc_allow`) VALUES (1, 2, 'D', 1);
-INSERT INTO `basedb`.`acl_access` (`acc_resource`, `acc_role`, `acc_privilege`, `acc_allow`) VALUES (1, 2, 'R', 1);
-INSERT INTO `basedb`.`acl_access` (`acc_resource`, `acc_role`, `acc_privilege`, `acc_allow`) VALUES (1, 2, 'U', 1);
-INSERT INTO `basedb`.`acl_access` (`acc_resource`, `acc_role`, `acc_privilege`, `acc_allow`) VALUES (2, 2, 'U', 0);
-INSERT INTO `basedb`.`acl_access` (`acc_resource`, `acc_role`, `acc_privilege`, `acc_allow`) VALUES (3, 1, 'R', 1);
-INSERT INTO `basedb`.`acl_access` (`acc_resource`, `acc_role`, `acc_privilege`, `acc_allow`) VALUES (3, 2, 'R', 1);
-INSERT INTO `basedb`.`acl_access` (`acc_resource`, `acc_role`, `acc_privilege`, `acc_allow`) VALUES (4, 1, 'U', 1);
-INSERT INTO `basedb`.`acl_access` (`acc_resource`, `acc_role`, `acc_privilege`, `acc_allow`) VALUES (4, 2, 'U', 0);
+USE `rtproject`;
+INSERT INTO `rtproject`.`acl_access` (`acc_resource`, `acc_role`, `acc_privilege`, `acc_allow`) VALUES (1, 1, 'A', 1);
+INSERT INTO `rtproject`.`acl_access` (`acc_resource`, `acc_role`, `acc_privilege`, `acc_allow`) VALUES (1, 1, 'C', 1);
+INSERT INTO `rtproject`.`acl_access` (`acc_resource`, `acc_role`, `acc_privilege`, `acc_allow`) VALUES (1, 1, 'D', 1);
+INSERT INTO `rtproject`.`acl_access` (`acc_resource`, `acc_role`, `acc_privilege`, `acc_allow`) VALUES (1, 1, 'R', 1);
+INSERT INTO `rtproject`.`acl_access` (`acc_resource`, `acc_role`, `acc_privilege`, `acc_allow`) VALUES (1, 1, 'U', 1);
+INSERT INTO `rtproject`.`acl_access` (`acc_resource`, `acc_role`, `acc_privilege`, `acc_allow`) VALUES (2, 1, 'U', 1);
+INSERT INTO `rtproject`.`acl_access` (`acc_resource`, `acc_role`, `acc_privilege`, `acc_allow`) VALUES (1, 2, 'A', 1);
+INSERT INTO `rtproject`.`acl_access` (`acc_resource`, `acc_role`, `acc_privilege`, `acc_allow`) VALUES (1, 2, 'C', 1);
+INSERT INTO `rtproject`.`acl_access` (`acc_resource`, `acc_role`, `acc_privilege`, `acc_allow`) VALUES (1, 2, 'D', 1);
+INSERT INTO `rtproject`.`acl_access` (`acc_resource`, `acc_role`, `acc_privilege`, `acc_allow`) VALUES (1, 2, 'R', 1);
+INSERT INTO `rtproject`.`acl_access` (`acc_resource`, `acc_role`, `acc_privilege`, `acc_allow`) VALUES (1, 2, 'U', 1);
+INSERT INTO `rtproject`.`acl_access` (`acc_resource`, `acc_role`, `acc_privilege`, `acc_allow`) VALUES (2, 2, 'U', 0);
+INSERT INTO `rtproject`.`acl_access` (`acc_resource`, `acc_role`, `acc_privilege`, `acc_allow`) VALUES (3, 1, 'R', 1);
+INSERT INTO `rtproject`.`acl_access` (`acc_resource`, `acc_role`, `acc_privilege`, `acc_allow`) VALUES (3, 2, 'R', 1);
+INSERT INTO `rtproject`.`acl_access` (`acc_resource`, `acc_role`, `acc_privilege`, `acc_allow`) VALUES (4, 1, 'U', 1);
+INSERT INTO `rtproject`.`acl_access` (`acc_resource`, `acc_role`, `acc_privilege`, `acc_allow`) VALUES (4, 2, 'U', 0);
 
 COMMIT;
 
